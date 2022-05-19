04c4b31ecca5bd96e8b0c976005d5ebfc27685e6
ahmed-razic <ahmed.razic@gmail.com>
2022-05-19 20:11:09 +0200

    google oauth

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,9 @@
-import { Navigate, useLocation, useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 import { doc, setDoc, getDoc, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase.config'
 import { toast } from 'react-toastify'
 import googleIcon from '../assets/svg/googleIcon.svg'
-import { db } from '../firebase.config'
 
 function OAuth() {
   const location = useLocation()
